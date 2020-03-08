e288036218c8a55cd92692df9911319f201dc637
jcichon <cichon.kuba@gmail.com>
2020-03-08 16:35:24 +0100

    First draft

---
+++ Python/Main.py
@@ -0,0 +1,12 @@
+import subprocess
+
+testdict = {}
+
+if __name__ == '__main__':
+
+    Subgroups = subprocess.check_output("powercfg -Q")
+    test = Subgroups.splitlines(False)
+    testdict = Subgroups.splitlines(False)
+    testdict.
+    None
+
