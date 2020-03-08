43f411de4ccb74a2d5a549382d5d17c027722713
jcichon <cichon.kuba@gmail.com>
2020-03-08 18:05:21 +0100

    Second draft

--- Python/Main.py
+++ Python/Main.py
@@ -1,12 +1,25 @@
 import subprocess
+import re
 
 testdict = {}
+
+
+def find_struct_level_in_string(self, text):
+    self.level = 0
+    for i in text:
+        while j
+
+
+
 
 if __name__ == '__main__':
 
     Subgroups = subprocess.check_output("powercfg -Q")
-    test = Subgroups.splitlines(False)
-    testdict = Subgroups.splitlines(False)
-    testdict.
+    test2 = re.split('\r\n', Subgroups)
+    for i in test2:
+        print(i)
+        print(i[0])
+
+
     None
 
